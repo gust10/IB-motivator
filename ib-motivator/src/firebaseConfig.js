e705b5ec14bc05e976194976e76cd92c7ef10cc4
Hyunsung Shin <2005hyunsung@gmail.com>
2024-08-15 03:04:08 +0800

    trying 2 :(

--- ib-motivator/src/firebaseConfig.js
+++ ib-motivator/src/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBC526ARBKrHei_8LASxVB1anIGbFkiqNU",
+  authDomain: "ib-motivator.firebaseapp.com",
+  databaseURL: "https://ib-motivator-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "ib-motivator",
+  storageBucket: "ib-motivator.appspot.com",
+  messagingSenderId: "467330705525",
+  appId: "1:467330705525:web:a53665b62db6dec4c1703c",
+  measurementId: "G-FRD8QR5XXF",
 };
 
 // Initialize Firebase
